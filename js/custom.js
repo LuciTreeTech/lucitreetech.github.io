8e4246b2c8a7033954dad4fa0ac2d7e82127b0ff
doannx <doannx@gmail.com>
2017-11-09 23:04:12 +0700

    Add bubble effect.

--- js/custom.js
+++ js/custom.js
@@ -65,6 +65,42 @@ $(function () {
         $('#lbl-service-title').html($.i18n('lbl-service-title'));
         $('#porfolio-title').html($.i18n('porfolio-title'));
     });
+
+    var mousePos = {};
+
+    function getRandomInt(min, max) {
+        return Math.round(Math.random() * (max - min + 1)) + min;
+    }
+
+    $(window).mousemove(function (e) {
+        mousePos.x = e.pageX;
+        mousePos.y = e.pageY;
+    });
+
+    $(window).mouseleave(function (e) {
+        mousePos.x = -1;
+        mousePos.y = -1;
+    });
+
+    var draw = setInterval(function () {
+        if (mousePos.x > 0 && mousePos.y > 0) {
+
+            var range = 15;
+
+            var color = "background: rgb(" + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + ");";
+
+            var sizeInt = getRandomInt(10, 30);
+            size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+
+            var left = "left: " + getRandomInt(mousePos.x - range - sizeInt, mousePos.x + range) + "px;";
+
+            var top = "top: " + getRandomInt(mousePos.y - range - sizeInt, mousePos.y + range) + "px;";
+
+            var style = left + top + color + size;
+            $("<div class='ball' style='" + style + "'></div>").appendTo('#top').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () { $(this).remove(); });
+        }
+    }, 10);
+
 });
 
 /* start preloader */
@@ -90,6 +126,6 @@ function changeLang(lang) {
 }
 
 function validEmail(email) { // see:
-  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-  return re.test(email);
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
 }
